4fcff69e9dd89d6bc92fcd69144279418122e1be
KhanTagiev <xan.2011@yandex.ru>
2021-08-26 17:56:10 +0300

    change puppeteer.launch options

--- utils/wbScrapper.js
+++ utils/wbScrapper.js
@@ -12,7 +12,7 @@ module.exports = class Scrapper {
   static isDtList = false;
 
   static async init() {
-    Scrapper.browser = await puppeteer.launch({ headless: false });
+    Scrapper.browser = await puppeteer.launch({ headless: true , args:['--no-sandbox']});
     Scrapper.page = await Scrapper.browser.newPage();
     await Scrapper.page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36');
   }
